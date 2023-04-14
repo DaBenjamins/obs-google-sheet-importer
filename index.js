b78be41e984be99bce59a9f650e94776eff0b174
DaBenjamins <mike@benja.cc>
2023-04-14 11:20:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -27,137 +27,137 @@ const update = async (obs) => {
 				
 		console.log("Sheets Updated");
   			
-	  const range = config.range;
-	  const startcell = range.split(":")[0].trim();
+		const range = config.range;
+		const startcell = range.split(":")[0].trim();
 
-	  const startcol = startcell.match("[a-zA-Z]+");
-	  //console.log("starting column is " + startcol);
-	  const startrow = startcell.match("[0-9]+");
-	  //console.log("starting row is " + startrow);
+		const startcol = startcell.match("[a-zA-Z]+");
+		//console.log("starting column is " + startcol);
+		const startrow = startcell.match("[0-9]+");
+		//console.log("starting row is " + startrow);
 
-	  const rowoffset = startrow[0];
-	  //console.log("row offset to array is " + rowoffset);
-	  const coloffset = columnToNumber(startcol[0]);
-	  //console.log("colum offset to array is " + coloffset);
+		const rowoffset = startrow[0];
+		//console.log("row offset to array is " + rowoffset);
+		const coloffset = columnToNumber(startcol[0]);
+		//console.log("colum offset to array is " + coloffset);
 
-	  const sceneList = await obs.send('GetSceneList');
-	  await sceneList.scenes.forEach(async scene => {
-		// unfold group children
-		const allSources = getChildren(scene.sources);
+		const sceneList = await obs.send('GetSceneList');
+		await sceneList.scenes.forEach(async scene => {
+			// unfold group children
+			const allSources = getChildren(scene.sources);
 
-		// console.log(scene);
-		await allSources.forEach(async source => {
-		  if (source.name.includes('|sheet')) {
-			const reference = source.name.split('|sheet')[1].trim();
+			// console.log(scene);
+			await allSources.forEach(async source => {
+			  if (source.name.includes('|sheet')) {
+				const reference = source.name.split('|sheet')[1].trim();
 
-			let col = reference.match("[a-zA-Z]+");
-			let colnumber = 3//columnToNumber(col[0]) - coloffset;
-			
-			let row = reference.match("[0-9]+");
-			let rownumber = row[0] - rowoffset;
-
-			let cellvalue = data[colnumber][rownumber];
-			console.log("Value for cell in source is " + cellvalue)
-			
-			let sourcetype = data[2][rownumber]
-			// If Source type is Text
-			if (cellvalue != undefined) {
+				let col = reference.match("[a-zA-Z]+");
+				let colnumber = 3//columnToNumber(col[0]) - coloffset;
 				
-				if (sourcetype == "Text"){
-					
-				  let color = null;
+				let row = reference.match("[0-9]+");
+				let rownumber = row[0] - rowoffset;
 
-				  if (cellvalue.startsWith('?color')) {
-					const split = cellvalue.split(';');
-					cellvalue = split[1];
-					color = split[0].split('=')[1];
-					color = color.replace('#', '');
-					const color1 = color.substring(0, 2);
-					const color2 = color.substring(2, 4);
-					const color3 = color.substring(4, 6);
-					color = parseInt('ff' + color3 + color2 + color1, 16);
-				  }
+				let cellvalue = data[colnumber][rownumber];
+				console.log("Value for cell in source is " + cellvalue)
+				
+				let sourcetype = data[2][rownumber]
+				// If Source type is Text
+				if (cellvalue != undefined) {
+					
+					if (sourcetype == "Text"){
+						
+					  let color = null;
 
-				  if (cellvalue.startsWith('?hide')) {
-					const split = cellvalue.split(';');
-					cellvalue = split[1];
-					await obs.send("SetSceneItemRender", {
-					  'scene-name': scene.name,
-					  source: source.name,
-					  render: false
-					});
-				  } else if (cellvalue.startsWith('?show')) {
-					const split = cellvalue.split(';');
-					cellvalue = split[1];
-					await obs.send("SetSceneItemRender", {
-					  'scene-name': scene.name,
-					  source: source.name,
-					  render: true
-					});
-				  }
-				  
-				  
-				  // Update to OBS
-				  await obs.send("SetTextGDIPlusProperties", {
-					source: source.name,
-					text: cellvalue,
-					color: color
-				  });
-				  console.log(`Updated: ${reference} to OBS: ${source.name}`);
-					
-				}
-			
-				// If Source type is Color
-				if (sourcetype == "Color"){
-					let color = null;
-					color = cellvalue
-					color = color.replace('#', '');
-					const color1 = color.substring(0, 2);
-					const color2 = color.substring(2, 4);
-					const color3 = color.substring(4, 6);
-					color = parseInt('ff' + color3 + color2 + color1, 16);
-								
-					await obs.send("SetSourceSettings", {
-						sourceName: source.name,
-						sourceType: source.type,
-						sourceSettings: {
-							color: color
-						}
-					});	
-					
-				}
-				if (sourcetype == "Image"){
-					
-					let imagesettings = await obs.send("GetSourceSettings", {
-						sourceName: source.name,
-						sourceType: source.type,
-					});	
-					//console.log(imagesettings);
-					let oldfile = imagesettings['sourceSettings']['file']
-					//console.log(oldfile);
-					if (cellvalue != oldfile){
-						console.log('image updated');
+					  if (cellvalue.startsWith('?color')) {
+						const split = cellvalue.split(';');
+						cellvalue = split[1];
+						color = split[0].split('=')[1];
+						color = color.replace('#', '');
+						const color1 = color.substring(0, 2);
+						const color2 = color.substring(2, 4);
+						const color3 = color.substring(4, 6);
+						color = parseInt('ff' + color3 + color2 + color1, 16);
+					  }
+
+					  if (cellvalue.startsWith('?hide')) {
+						const split = cellvalue.split(';');
+						cellvalue = split[1];
+						await obs.send("SetSceneItemRender", {
+						  'scene-name': scene.name,
+						  source: source.name,
+						  render: false
+						});
+					  } else if (cellvalue.startsWith('?show')) {
+						const split = cellvalue.split(';');
+						cellvalue = split[1];
+						await obs.send("SetSceneItemRender", {
+						  'scene-name': scene.name,
+						  source: source.name,
+						  render: true
+						});
+					  }
+					  
+					  
+					  // Update to OBS
+					  await obs.send("SetTextGDIPlusProperties", {
+						source: source.name,
+						text: cellvalue,
+						color: color
+					  });
+					  console.log(`Updated: ${reference} to OBS: ${source.name}`);
+						
+					}
+				
+					// If Source type is Color
+					if (sourcetype == "Color"){
+						let color = null;
+						color = cellvalue
+						color = color.replace('#', '');
+						const color1 = color.substring(0, 2);
+						const color2 = color.substring(2, 4);
+						const color3 = color.substring(4, 6);
+						color = parseInt('ff' + color3 + color2 + color1, 16);
+									
 						await obs.send("SetSourceSettings", {
 							sourceName: source.name,
 							sourceType: source.type,
 							sourceSettings: {
-								file: cellvalue
+								color: color
 							}
 						});	
-					}			
+						
+					}
+					if (sourcetype == "Image"){
+						
+						let imagesettings = await obs.send("GetSourceSettings", {
+							sourceName: source.name,
+							sourceType: source.type,
+						});	
+						//console.log(imagesettings);
+						let oldfile = imagesettings['sourceSettings']['file']
+						//console.log(oldfile);
+						if (cellvalue != oldfile){
+							console.log('image updated');
+							await obs.send("SetSourceSettings", {
+								sourceName: source.name,
+								sourceType: source.type,
+								sourceSettings: {
+									file: cellvalue
+								}
+							});	
+						}			
+					}
+					if (sourcetype == "Browser"){
+						
+						await obs.send("SetBrowserSourceProperties", {
+							source: source.name,
+							url: cellvalue
+						});	
+						
+					}
 				}
-				if (sourcetype == "Browser"){
-					
-					await obs.send("SetBrowserSourceProperties", {
-						source: source.name,
-						url: cellvalue
-					});	
-					
-				}
-			}
-		  }
+			  }
+			});
 		});
-	  });
 
 		const fs = require('fs');
 		const jsonContent = JSON.stringify(data);
@@ -166,7 +166,6 @@ const update = async (obs) => {
 			if (err) {
 				return console.log(err);
 			}
-
 			console.log("The file was saved!");
 		}); 
 	}
